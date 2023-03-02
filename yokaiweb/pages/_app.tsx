7ef493ea1e5b2bf8bebe5843997eb449687b4c8f
dylviz <dylanvizcarra@gmail.com>
2023-03-02 10:58:13 -0800

    saving to modify computer

---
+++ yokaiweb/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { myTheme } from "@/styles/theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={myTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
---
+++ yokaiweb/styles/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react"
+import { mode } from "@chakra-ui/theme-tools";
+
+export const myTheme = extendTheme({
+    colors:{
+        yokai: {
+            primary : "#CB0000",
+        },
+    },
+});
